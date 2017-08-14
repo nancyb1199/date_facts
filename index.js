1004260e480c44a372d268db8585223c85b53817
Nancy <belscher@Nancys-MacBook-Pro.local>
2017-08-14 15:01:04 -0400

    working with colors

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// What is today
+
 var moment = require('moment');
 moment().format();
+const chalk = require('chalk');
 
+// What is today
 let now = moment();
 
-console.log(now.format('"dddd, MMMM Do YYYY, h:mm:ss a"'));
+console.log('It is ' + chalk.blue(now.format('dddd, MMMM Do YYYY, h:mm:ss a')));
 
 // Calculate day of the day
 let day = moment().format("DDDo");
 
-console.log(`It is the ${day} day of the year.`);
+console.log('It is the ' + chalk.magenta(`${day}`) + ' day of the year.');
 
 // Calculate seconds in a day
 let hour = moment().hour();
@@ -17,23 +19,23 @@ let minute = moment().minute();
 let second = moment().second();
 
 let answer = ((hour*60)*60) + (minute * 60) + second;
-console.log(`It is ${answer} seconds into the day`);
+console.log('It is ' + chalk.cyan(`${answer}`) + ' seconds into the day');
 
 // Daylight savings time?
 let dst = moment().isDST();
 
 if(dst){
-  console.log("It is during Daylight Savings Time");
+  console.log("It " + chalk.green("is") + " during Daylight Savings Time");
 }
 else {
-  console.log("It is not during Daylight Savings Time");
+  console.log("It " + chalk.red("is not") +  " during Daylight Savings Time");
 }
 
 // Leap Year?
 let ly = moment().isLeapYear();
 if(ly){
-  console.log("It is a leap year");
+  console.log("It " + chalk.green("is") + " a leap year");
 }
 else {
-  console.log("It is not a leap year");
+  console.log("It " + chalk.red("is not") +  " a leap year");
 }

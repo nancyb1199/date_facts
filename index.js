5201fafe3b74a2b8d69655c8fecbdad31ea097fa
Nancy <belscher@Nancys-MacBook-Pro.local>
2017-08-14 14:28:25 -0400

    first 3 questions working

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ moment().format();
 let now = moment();
 
 console.log(now.format('"dddd, MMMM Do YYYY, h:mm:ss a"'));
+
+let day = moment().dayOfYear();
+
+console.log(`It is the ${day} day of the year.`);
+
+let hour = moment().hour();
+let minute = moment().minute();
+let second = moment().second();
+
+let answer = ((hour*60)*60) + (minute * 60) + second;
+console.log(`It is ${answer} seconds into the day`);

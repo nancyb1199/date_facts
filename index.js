dffd9f939c37490b083f1b4c39db71622b2e3b81
Nancy <belscher@Nancys-MacBook-Pro.local>
2017-08-14 14:34:24 -0400

    all moment functions working now need to add chalk

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// What is today
 var moment = require('moment');
 moment().format();
 
@@ -5,13 +6,34 @@ let now = moment();
 
 console.log(now.format('"dddd, MMMM Do YYYY, h:mm:ss a"'));
 
+// Calculate day of the day
 let day = moment().dayOfYear();
 
 console.log(`It is the ${day} day of the year.`);
 
+// Calculate seconds in a day
 let hour = moment().hour();
 let minute = moment().minute();
 let second = moment().second();
 
 let answer = ((hour*60)*60) + (minute * 60) + second;
 console.log(`It is ${answer} seconds into the day`);
+
+// Daylight savings time?
+let dst = moment().isDST();
+
+if(dst){
+  console.log("It is during Daylight Savings Time");
+}
+else {
+  console.log("It is not during Daylight Savings Time");
+}
+
+// Leap Year?
+let ly = moment().isLeapYear();
+if(ly){
+  console.log("It is a leap year");
+}
+else {
+  console.log("It is not a leap year");
+}

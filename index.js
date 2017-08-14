69571075bf2536c011aa4d18fd5dbdc13d920277
Nancy <belscher@Nancys-MacBook-Pro.local>
2017-08-14 15:28:45 -0400

    added punctuation

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const chalk = require('chalk');
 // What is today
 let now = moment();
 
-console.log('It is ' + chalk.blue(now.format('dddd, MMMM Do YYYY, h:mm:ss a')));
+console.log('It is ' + chalk.blue(now.format('dddd, MMMM Do YYYY, h:mm:ss a')) + ".");
 
 // Calculate day of the day
 let day = moment().format("DDDo");
@@ -19,23 +19,23 @@ let minute = moment().minute();
 let second = moment().second();
 
 let answer = ((hour*60)*60) + (minute * 60) + second;
-console.log('It is ' + chalk.cyan(`${answer}`) + ' seconds into the day');
+console.log('It is ' + chalk.cyan(`${answer}`) + ' seconds into the day.');
 
 // Daylight savings time?
 let dst = moment().isDST();
 
 if(dst){
-  console.log("It " + chalk.green("is") + " during Daylight Savings Time");
+  console.log("It " + chalk.green("is") + " during Daylight Savings Time.");
 }
 else {
-  console.log("It " + chalk.red("is not") +  " during Daylight Savings Time");
+  console.log("It " + chalk.red("is not") +  " during Daylight Savings Time.");
 }
 
 // Leap Year?
 let ly = moment().isLeapYear();
 if(ly){
-  console.log("It " + chalk.green("is") + " a leap year");
+  console.log("It " + chalk.green("is") + " a leap year.");
 }
 else {
-  console.log("It " + chalk.red("is not") +  " a leap year");
+  console.log("It " + chalk.red("is not") +  " a leap year.");
 }
